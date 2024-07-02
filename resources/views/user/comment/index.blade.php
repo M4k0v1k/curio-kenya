61836ad577196c4a8427b3bf961c4d332944a3e7
Jasmeet Singh Mudher <110900189+JasmeetMudher@users.noreply.github.com>
2024-07-02 12:34:56 +0300

    Updated

--- resources/views/user/comment/index.blade.php
+++ resources/views/user/comment/index.blade.php
@@ -1,7 +1,6 @@
 @extends('user.layouts.master')
 @section('title','KenyaCurio || Comment Page')
 @section('main-content')
- <!-- DataTales Example -->
  <div class="card shadow mb-4">
      <div class="row">
          <div class="col-md-12">
