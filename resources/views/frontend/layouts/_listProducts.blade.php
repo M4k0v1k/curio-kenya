361e32d6284ad6ba45a3ea737a08e6c6bfe82b31
M4k0v1k <nathan.ng'ang'a@strathmore.edu>
2024-07-14 15:51:57 +0300

    product

--- resources/views/frontend/layouts/_listProducts.blade.php
+++ resources/views/frontend/layouts/_listProducts.blade.php
@@ -23,7 +23,7 @@
                     <div class="product-content">
                         <h3><a href="product-details.html">Women Hot Collection</a></h3>
                         <div class="product-price">
-                            <span>$29.00</span>
+                            <span>KSH29.00</span>
                         </div>
                     </div>
                 </div>
@@ -75,7 +75,7 @@
                     <div class="product-content">
                         <h3><a href="product-details.html">Awesome Bags Collection</a></h3>
                         <div class="product-price">
-                            <span>$29.00</span>
+                            <span>KSH29.00</span>
                         </div>
                     </div>
                 </div>
